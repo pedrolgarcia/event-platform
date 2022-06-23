ff4b0bb0353ba798c61554aefaf079a7be31a68e
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-22 23:51:38 -0300

    feat: creating structure components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import { useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
-
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  // useEffect(() => {
-  //   client
-  //     .query({ query: GET_LESSONS_QUERY })
-  //     .then((response) => console.log(response.data));
-  // }, []);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
-  );
+  return <Event />;
 }
 
 export default App;
