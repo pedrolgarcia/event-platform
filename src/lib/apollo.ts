775dbeaff02c14c9dd794e75f1890b639199b9af
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-06-28 22:48:48 -0300

    feat: mutation create subscriber

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4oy1c6h1d9p01z459zxbp17/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 });
